a1d954e47df72dcddd4d56e1e76383d6319fe6a2
azad barkho <azadbarkho@gmail.com>
2021-07-12 11:34:47 +0200

    kommentar hinzugefügt

--- Hello world/app.js
+++ Hello world/app.js
@@ -1,4 +1,4 @@
-new Vue({ 
+new Vue({//das ist ein Vue instance.  es manipuliert und kontrolliert DOM(Document Object Model) in diesem Beispiel vom DIV-Tag mit ID 'app' 
   el:'#app',
   data:{
       content: 'Hello world',
